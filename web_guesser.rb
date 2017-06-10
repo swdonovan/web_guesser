ddcd38901f468a7ab0f37375a401630ab30d4946
Wil_D <wil.donovan@gmail.com>
2017-06-10 09:59:09 -0600

    I1: generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+secret_number = rand(99)
 
 get '/' do
-  "Hello, World!"
+   "The secret number is #{secret_number}".upcase!
+end
+
+
+class WebGuesser
+  attr_reader :secret_number
+
+  def initialize
+    secret_number = rand(99)
+    puts "The secret number is #{secret_number}"
+  end
 end
