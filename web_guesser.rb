80a0fe54ecc13e64e62428832d35d53805ebe84a
Wil_D <wil.donovan@gmail.com>
2017-06-12 18:25:41 -0500

    html build out and secret number holding

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,15 +4,5 @@ require 'sinatra/reloader'
 secret_number = rand(99)
 
 get '/' do
-   "The secret number is #{secret_number}".upcase!
-end
-
-
-class WebGuesser
-  attr_reader :secret_number
-
-  def initialize
-    secret_number = rand(99)
-    puts "The secret number is #{secret_number}"
-  end
+  erb :index, :locals => {:secret_number => secret_number }
 end
